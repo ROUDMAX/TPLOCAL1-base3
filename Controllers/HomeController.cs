fb6223bc158de85606add11ca86579dfc17055ef
ROUDMAX <63471368+ROUDMAX@users.noreply.github.com>
2023-02-02 16:48:22 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using TPLOCAL1_base.Models;
+
+//L'énoncé du tp et le logo hn sont livrés dans le répertoire /ressources de la solution
+//--------------------------------------------------------------------------------------
+//Attention, le modèle MVC est un modèle dit de convention plutot que configuration, 
+//Le controller doit donc obligatoirement se nommer avec "Controller" à la fin!!!
+namespace TPLOCAL1_base.Controllers
+{
+    public class HomeController : Controller
+    {
+        // méthode appelée par la routeur "naturellement"
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //renvoie vers la vue Index (voir routage dans RouteConfig.cs)
+                return View();
+            else
+            {
+                //en fonction du paramètre id, on appelle les différentes pages
+                switch (id)
+                {
+                    case "ListeAvis":
+                        //declarer une liste vide
+                        ListeAvis avisDonnes = new ListeAvis();
+                        // remplir et retourné cette liste 
+                        return View(id, avisDonnes.GetAvis("FichierXML/DataAvis.xml"));
+                    case "Formulaire":
+                        //Afficher la vue Formulaire avec le modèle de données vide
+                        return View(id);
+                    default:
+                        //renvoie vers Index (voir routage dans RouteConfig.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //méthode pour envoyer les données du formulaire vers la page de validation
+        [HttpPost]
+        public ActionResult ValidationFormulaire(FormulaireModel mod)
+        {
+            //Si l'utilisateur ne choissie pas le sexe 
+            if (mod.Sexe == "Sélectionner un sexe")
+            {
+                ModelState.AddModelError("", "Merci de vérifier le Sexe");
+            }
+            //Si l'utulisateur ne choissie pas de formation
+            if (mod.TypeFormation == "Sélectionner une formation")
+            {
+                ModelState.AddModelError("", "Merci de vérifier le Type de Formation");
+
+            }
+
+
+
+            if (!ModelState.IsValid)
+            {
+                return View("Formulaire");
+            }
+
+            return View(mod);
+
+        }
+    }
+}
---
+++ Models/FormulaireModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+
+namespace TPLOCAL1_base.Models
+{
+    public class FormulaireModel
+    {
+        [Display(Name = "Nom")]
+        [Required(ErrorMessage = "Merci de vérifier le Nom")]
+        public string Nom { get; set; }
+
+        [Display(Name = "Prénom")]
+        [Required(ErrorMessage = "Merci de vérifier le Prénom")]
+        public string Prenom { get; set; }
+        [Display(Name = "Sexe")]
+        [Required]
+        public string Sexe { get; set; }
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Merci de vérifier l'Adresse")]
+        public string Adresse { get; set; }
+
+        [Display(Name = "Code Postal")]
+        [Required(ErrorMessage = "Merci de vérifier le Code Postal")]
+        [RegularExpression(@"^[0-9]{5}$", ErrorMessage = "Code Postal doit être sur 5 caractères numériques ")]
+        public string Codepostal { get; set; }
+
+        [Display(Name = "Ville")]
+        [Required(ErrorMessage = "Merci de vérifier la Ville")]
+        public string Ville { get; set; }
+
+        [Display(Name = "Adresse Mail")]
+        [Required(ErrorMessage = "Merci de rensegné une adresse email valide")]
+        [RegularExpression(@"^([\w]+)@([\w]+)\.([\w]+)$")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Date début formation")]
+        [Required(ErrorMessage = "La date doit être inférieure au  01 / 01 / 2021")]
+        [Range(typeof(DateTime), "1/1/2011", "1/1/2021")]
+        public string DateFormation { get; set; }
+
+        [Display(Name = "Formation Suivie")]
+        [Required]
+        public string TypeFormation { get; set; }
+
+
+
+
+        [Display(Name = "Formation Cobol")]
+        public string Formationcobol { get; set; }
+
+        [Display(Name = "Formation C#")]
+        public string FormationC { get; set; }
+
+    }
+}
